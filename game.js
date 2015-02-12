a4b45d30d7f76cadb3d1867c0c21ff716b40290a
Victor Atteh <victoratteh@gmail.com>
2015-02-11 21:28:40 -0500

    added checkCollisions function

--- game.js
+++ game.js
@@ -5,6 +5,8 @@ function Tetris() {
 	this.currSpeed = speedLevels[0];
 	this.landedGrid = gameGrid;
 	this.currTetromino;
+	this.tetrominoRotations;
+	this.rotationIndex;
 	this.intervalID;
 }
 
@@ -16,46 +18,62 @@ Tetris.prototype.clearCanvas = function () {
 
 };
 
+Tetris.prototype.rotateTetromino = function () {
+	var prevTopLeft = this.currTetromino.topLeft;
+	this.currTetromino = this.tetrominoRotations.indexof(this.rotationIndex + 1) !== -1 ? this.tetrominoRotations[++this.rotationIndex] : this.tetrominoRotations[0]; 
+	this.currTetromino.topLeft = prevTopLeft;
+};
+
 Tetris.prototype.checkMove = function (keyCode) {
 
 	var potentialTopLeftRow;
 	var potentialTopLeftCol;
 
 	if (keyCode === 37) { // Left
-		potentialTopLeftRow = currTetromino.topLeft.row;
-		potentialTopLeftCol = currTetromino.topLeft.col - 1;
+		potentialTopLeftRow = this.currTetromino.topLeft.row;
+		potentialTopLeftCol = this.currTetromino.topLeft.col - 1;
 	} else if (keyCode === 39) { // Right
-		potentialTopLeftRow = currTetromino.topLeft.row;
-		potentialTopLeftCol = currTetromino.topLeft.col + 1;
+		potentialTopLeftRow = this.currTetromino.topLeft.row;
+		potentialTopLeftCol = this.currTetromino.topLeft.col + 1;
 	} else if (keyCode === 38) { // Down
-		potentialTopLeftRow = currTetromino.topLeft.row + 1;
-		potentialTopLeftCol = currTetromino.topLeft.col;
+		potentialTopLeftRow = this.currTetromino.topLeft.row + 1;
+		potentialTopLeftCol = this.currTetromino.topLeft.col;
 	} else {
 
 	}
 	
-	for (var row = 0; row < this.currTetromino.length; row++) {
-    	for (var col = 0; col < this.currTetromino[row].length; col++) {
+	if (this.checkCollisions(potentialTopLeftRow, potentialTopLeftCol, this.currTetromino)) {
+		// the Tetromino can move to new position 
+   		this.currTetromino.topLeft.row = potentialTopLeftRow;
+   		this.currTetromino.topLeft.col = potentialTopLeftCol;
+   		return true;
+	} else {
+		// the Tetromino cannot move to the left or right
+    	if (keyCode === 37 || keyCode === 39) {
+    		return;
+    	} else { // the Tetromino cannot move down so the shape will land
+    		this.landTetromino();
+    	}
 
-	        if ((this.currTetromino[row][col] != 0) &&  // is this block actually part of the Tetromino shape?
+    	return false;
+	}
+
+};
+
+Tetris.prototype.checkCollisions = function (potentialTopLeftRow, potentialTopLeftCol, potentialTetromino) {
+	for (var row = 0; row < potentialTetromino.length; row++) {
+    	for (var col = 0; col < potentialTetromino[row].length; col++) {
+
+	        if ((potentialTetromino[row][col] != 0) &&  // is this block actually part of the Tetromino shape?
 	        	((landedGrid[potentialTopLeftRow + row] === undefined || landedGrid[potentialTopLeftRow + row][potentialTopLeftCol + col] === undefined) || // is this block of the Tetromino now out of bounds?
 	        	(landedGrid[potentialTopLeftRow + row][potentialTopLeftCol + col] != 0))) { // is there already a landed block at this spot? 
 	                //the space is taken
-
-	            	// the Tetromino cannot move to the left or right
-	            	if (keyCode === 37 || keyCode === 39) {
-	            		return;
-	            	} else { // the Tetromino cannot move down so the shape will land
-	            		this.landTetromino();
-	            	}
+	            return false;
 	        }
 	    }
    	}
 
-   	// the Tetromino can move to new position 
-   	currTetromino.topLeft.row = potentialTopLeftRow;
-   	currTetromino.topLeft.col = potentialTopLeftCol;
-
+   	return true;
 };
 
 Tetris.prototype.landTetromino = function () {
@@ -65,8 +83,6 @@ Tetris.prototype.landTetromino = function () {
 	        	landedGrid[currTetromino.topLeft.row + row][currTetromino.topLeft.col + col] = this.currTetromino[row][col];
 	    }
    	}
-
-
 };
 
 Tetris.prototype.drawCanvas = function () {
