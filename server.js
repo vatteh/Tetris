6d64b91d2afb9b3eda9ebff3ed3ba90b2288ffef
vatteh <victor@artivest.co>
2017-07-31 23:27:51 -0400

    add server command

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 1337;
+
+app.use(express.static(path.join(__dirname, './')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, 'localhost', err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(`Server up at http://localhost:${port}`);
+});
