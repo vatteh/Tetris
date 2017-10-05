4bcea68f987982090f4660066d582e89d72df5b0
Victor Atteh <victor.atteh@datadoghq.com>
2017-10-04 23:17:53 -0400

    use createjs to render canvas

--- src/lib/render.js
+++ src/lib/render.js
@@ -1,14 +1,22 @@
+/* global createjs */
+
 class RenderEngine {
   constructor(game) {
-    this.canvas = document.getElementById('gameboard');
-    this.ctx = this.canvas.getContext('2d');
     this.game = game;
     this.BLOCK_WIDTH = 30;
     this.BLOCK_HEIGHT = 30;
     this.BOARD_WIDTH = this.BLOCK_WIDTH * this.game.width;
     this.BOARD_HEIGHT = this.BLOCK_HEIGHT * this.game.height;
-    this.canvas.setAttribute('width', this.BOARD_WIDTH);
-    this.canvas.setAttribute('height', this.BOARD_HEIGHT);
+    this.stage = new createjs.Stage('gameboard');
+    this.stage.canvas.width = this.BOARD_WIDTH;
+    this.stage.canvas.height = this.BOARD_HEIGHT;
+
+    this.NEXT_BOARD_WIDTH = this.BLOCK_WIDTH * 5;
+    this.NEXT_BOARD_HEIGHT = this.BLOCK_HEIGHT * 5;
+    this.nextStage = new createjs.Stage('nextboard');
+    this.nextStage.canvas.width = this.NEXT_BOARD_WIDTH;
+    this.nextStage.canvas.height = this.NEXT_BOARD_HEIGHT;
+
     this.tetrominoColors = [
       ['', '', ''],
       ['rgb(0,201,255)', 'rgb(56,255,255)', 'rgb(0,224,255)', 'rgb(100, 240, 253)'], // cyan
@@ -19,62 +27,62 @@ class RenderEngine {
       ['rgb(127,63,152)', 'rgb(235,184,255)', 'rgb(201,115,255)', 'rgb(213, 148, 252)'], // purple
       ['rgb(190,30,45)', 'rgb(255,148,150)', 'rgb(255,36,55)', 'rgb(254, 102, 115)'], // red
     ]; // dark, light, base
-
-    this.nextBoard = document.getElementById('nextboard');
-    this.nextCtx = this.nextBoard.getContext('2d');
-    this.NEXT_BOARD_WIDTH = this.BLOCK_WIDTH * 5;
-    this.NEXT_BOARD_HEIGHT = this.BLOCK_HEIGHT * 5;
-    this.nextBoard.setAttribute('width', this.NEXT_BOARD_WIDTH);
-    this.nextBoard.setAttribute('height', this.NEXT_BOARD_HEIGHT);
   }
 
   // draw a single square at (x, y)
-  drawBlock(i, j, colorArray, ctx) {
+  drawBlock(i, j, colorArray, stage, strokeStyle) {
     // light
-    ctx.lineWidth = 1.5;
-    ctx.lineJoin = 'round';
-    ctx.fillStyle = colorArray[1];
-    ctx.fillRect(this.BLOCK_WIDTH * j, this.BLOCK_HEIGHT * i, this.BLOCK_WIDTH - 1, this.BLOCK_HEIGHT - 1);
+    const light = new createjs.Shape();
+    light.graphics
+      .beginFill(colorArray[1])
+      .drawRect(this.BLOCK_WIDTH * j, this.BLOCK_HEIGHT * i, this.BLOCK_WIDTH - 1, this.BLOCK_HEIGHT - 1);
+    stage.addChild(light);
 
     // dark
-    ctx.fillStyle = colorArray[0];
-    ctx.beginPath();
-    ctx.moveTo(this.BLOCK_WIDTH * j, this.BLOCK_HEIGHT * i);
-    ctx.lineTo(this.BLOCK_WIDTH * j, this.BLOCK_HEIGHT * i + this.BLOCK_HEIGHT - 1);
-    ctx.lineTo(this.BLOCK_WIDTH * j + this.BLOCK_WIDTH - 1, this.BLOCK_HEIGHT * i + this.BLOCK_HEIGHT - 1);
-    ctx.fill();
+    const dark = new createjs.Shape();
+    dark.graphics
+      .setStrokeStyle(1)
+      .beginFill(colorArray[0])
+      .moveTo(this.BLOCK_WIDTH * j, this.BLOCK_HEIGHT * i)
+      .lineTo(this.BLOCK_WIDTH * j, this.BLOCK_HEIGHT * i + this.BLOCK_HEIGHT - 1)
+      .lineTo(this.BLOCK_WIDTH * j + this.BLOCK_WIDTH - 1, this.BLOCK_HEIGHT * i + this.BLOCK_HEIGHT - 1)
+      .endFill();
+    stage.addChild(dark);
 
     // base
     const bevelThickness = this.BLOCK_WIDTH / 6;
-    ctx.fillStyle = colorArray[2];
-    ctx.fillRect(
-      this.BLOCK_WIDTH * j + bevelThickness,
-      this.BLOCK_HEIGHT * i + bevelThickness,
-      this.BLOCK_WIDTH - 1 - bevelThickness * 2,
-      this.BLOCK_HEIGHT - 1 - bevelThickness * 2,
-    );
-    ctx.strokeRect(this.BLOCK_WIDTH * j, this.BLOCK_HEIGHT * i, this.BLOCK_WIDTH - 1, this.BLOCK_HEIGHT - 1);
+    const base = new createjs.Shape();
+    base.graphics
+      .beginFill(colorArray[2])
+      .drawRect(
+        this.BLOCK_WIDTH * j + bevelThickness,
+        this.BLOCK_HEIGHT * i + bevelThickness,
+        this.BLOCK_WIDTH - 1 - bevelThickness * 2,
+        this.BLOCK_HEIGHT - 1 - bevelThickness * 2,
+      );
+    stage.addChild(base);
 
     // highlight
     const highlightThickness = bevelThickness / 2;
-    ctx.lineWidth = 0.5;
-    ctx.fillStyle = colorArray[3];
-    ctx.beginPath();
-    ctx.moveTo(this.BLOCK_WIDTH * j + bevelThickness, this.BLOCK_HEIGHT * i + bevelThickness);
-    ctx.lineTo(
-      this.BLOCK_WIDTH * j + bevelThickness,
-      this.BLOCK_HEIGHT * i + bevelThickness + (this.BLOCK_HEIGHT - bevelThickness * 2),
-    );
-    ctx.lineTo(
-      this.BLOCK_WIDTH * j + bevelThickness + (this.BLOCK_WIDTH - bevelThickness * 2),
-      this.BLOCK_HEIGHT * i + bevelThickness + (this.BLOCK_HEIGHT - bevelThickness * 2),
-    );
-    ctx.lineTo(
-      this.BLOCK_WIDTH * j + bevelThickness + highlightThickness,
-      this.BLOCK_HEIGHT * i + bevelThickness + (this.BLOCK_HEIGHT - bevelThickness * 2) - highlightThickness,
-    );
-    ctx.closePath();
-    ctx.fill();
+    const highlight = new createjs.Shape();
+    highlight.graphics
+      .setStrokeStyle(0.5)
+      .beginFill(colorArray[3])
+      .moveTo(this.BLOCK_WIDTH * j + bevelThickness, this.BLOCK_HEIGHT * i + bevelThickness)
+      .lineTo(
+        this.BLOCK_WIDTH * j + bevelThickness,
+        this.BLOCK_HEIGHT * i + bevelThickness + (this.BLOCK_HEIGHT - bevelThickness * 2),
+      )
+      .lineTo(
+        this.BLOCK_WIDTH * j + bevelThickness + (this.BLOCK_WIDTH - bevelThickness * 2),
+        this.BLOCK_HEIGHT * i + bevelThickness + (this.BLOCK_HEIGHT - bevelThickness * 2),
+      )
+      .lineTo(
+        this.BLOCK_WIDTH * j + bevelThickness + highlightThickness,
+        this.BLOCK_HEIGHT * i + bevelThickness + (this.BLOCK_HEIGHT - bevelThickness * 2) - highlightThickness,
+      )
+      .endFill();
+    stage.addChild(highlight);
   }
 
   renderGhost() {
@@ -93,26 +101,25 @@ class RenderEngine {
     }
 
     // render the current Tetromino
-    this.ctx.lineWidth = 1.5;
-    this.ctx.lineJoin = 'round';
-    this.ctx.fillStyle = 'grey';
     for (let i = 0; i < this.game.currTetromino.length; i++) {
       for (let j = 0; j < this.game.currTetromino[0].length; j++) {
         if (this.game.currTetromino[i][j]) {
-          this.ctx.fillRect(
-            this.BLOCK_WIDTH * (j + ghostTopLeftCol) + 1,
-            this.BLOCK_HEIGHT * (i + ghostTopLeftRow) + 1,
-            this.BLOCK_WIDTH - 3,
-            this.BLOCK_HEIGHT - 3,
-          );
+          const block = new createjs.Shape();
+          block.graphics
+            .beginFill('grey')
+            .drawRect(
+              this.BLOCK_WIDTH * (j + ghostTopLeftCol) + 1,
+              this.BLOCK_HEIGHT * (i + ghostTopLeftRow) + 1,
+              this.BLOCK_WIDTH - 3,
+              this.BLOCK_HEIGHT - 3,
+            );
         }
       }
     }
   }
 
   renderNextBoard() {
-    this.nextCtx.clearRect(0, 0, this.NEXT_BOARD_WIDTH, this.NEXT_BOARD_HEIGHT);
-
+    this.nextStage.removeAllChildren();
     // calculate the middle of this tetriminto
     const nextTetrominoMiddleH = this.game.nextTetromino.length / 2;
     const nextTetrominoMiddleW = this.game.nextTetromino[0].length / 2;
@@ -124,7 +131,6 @@ class RenderEngine {
     const widthOffset = nextBoardMiddleW - nextTetrominoMiddleW;
 
     // render the next board
-    this.nextCtx.strokeStyle = 'white';
     for (let i = 0; i < this.game.nextTetromino.length; i++) {
       for (let j = 0; j < this.game.nextTetromino[0].length; j++) {
         if (this.game.nextTetromino[i][j]) {
@@ -132,30 +138,30 @@ class RenderEngine {
             heightOffset + i,
             widthOffset + j,
             this.tetrominoColors[this.game.nextTetromino[i][j]],
-            this.nextCtx,
+            this.nextStage,
+            'white',
           );
         }
       }
     }
+
+    this.nextStage.update();
   }
 
   // draws the landed board and the current Tetromino
   render() {
-    this.ctx.clearRect(0, 0, this.BOARD_WIDTH, this.BOARD_HEIGHT);
-
+    this.stage.removeAllChildren();
     this.renderGhost();
     // render the landed grid
-    this.ctx.strokeStyle = 'black';
     for (let i = 0; i < this.game.landedGrid.length; i++) {
       for (let j = 0; j < this.game.landedGrid[0].length; j++) {
         if (this.game.landedGrid[i][j]) {
-          this.drawBlock(i, j, this.tetrominoColors[this.game.landedGrid[i][j]], this.ctx);
+          this.drawBlock(i, j, this.tetrominoColors[this.game.landedGrid[i][j]], this.stage, 'black');
         }
       }
     }
 
     // render the current Tetromino
-    this.ctx.strokeStyle = 'white';
     for (let i = 0; i < this.game.currTetromino.length; i++) {
       for (let j = 0; j < this.game.currTetromino[0].length; j++) {
         if (this.game.currTetromino[i][j]) {
@@ -163,11 +169,14 @@ class RenderEngine {
             this.game.currTetromino.topLeft.row + i,
             this.game.currTetromino.topLeft.col + j,
             this.tetrominoColors[this.game.currTetromino[i][j]],
-            this.ctx,
+            this.stage,
+            'white',
           );
         }
       }
     }
+
+    this.stage.update();
   }
 }
 
